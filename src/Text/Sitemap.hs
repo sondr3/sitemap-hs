74608eabd5ae0f5f3a0c76bf39e360312ee0a310
Sondre Aasemoen <sondre@eons.io>
2022-07-13 20:05:43 +0200

    Add XML renderer for sitemaps with xml-conduit

--- src/Text/Sitemap.hs
+++ src/Text/Sitemap.hs
@@ -9,12 +9,22 @@ module Text.Sitemap
     nullSitemapEntry,
     newSitemap,
     newSitemapIndex,
+
+    -- * Rendering
+    buildSitemap,
+    renderSitemap,
+    renderSitemapWith,
   )
 where
 
+import Data.Maybe (catMaybes)
 import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as L
 import Data.Time (UTCTime)
-import Data.XML.Types as XML
+import Data.Time.Format.ISO8601 (ISO8601 (iso8601Format), formatShow)
+import qualified Data.XML.Types as X
+import qualified Text.XML as XML
 
 data ChangeFrequency
   = Always
@@ -53,9 +63,6 @@ data SitemapIndex = SitemapIndex
   }
   deriving stock (Show, Eq, Ord)
 
-sitemapNamespace :: Text
-sitemapNamespace = "://www.sitemaps.org/schemas/sitemap/0.9"
-
 nullSitemapEntry :: Text -> SitemapEntry
 nullSitemapEntry url = SitemapEntry {loc = url, lastModified = Nothing, changeFreq = Nothing, priority = Nothing}
 
@@ -64,3 +71,24 @@ newSitemap urls = Sitemap {urls}
 
 newSitemapIndex :: [Sitemap] -> SitemapIndex
 newSitemapIndex sitemaps = SitemapIndex {sitemaps}
+
+entryToXML :: SitemapEntry -> X.Node
+entryToXML entry = X.NodeElement $ X.Element "url" [] (map X.NodeElement $ catMaybes [locXML, lastModXML, changeFreqXML, priorityXML])
+  where
+    locXML = Just $ X.Element "loc" [] [X.NodeContent $ X.ContentText $ loc entry]
+    lastModXML = maybe Nothing (\x -> Just $ X.Element "lastmod" [] [X.NodeContent $ X.ContentText $ T.pack $ formatShow iso8601Format x]) (lastModified entry)
+    changeFreqXML = maybe Nothing (\x -> Just $ X.Element "changeFreq" [] [X.NodeContent $ X.ContentText $ changeFrequencyToText x]) (changeFreq entry)
+    priorityXML = maybe Nothing (\x -> Just $ X.Element "changeFreq" [] [X.NodeContent $ X.ContentText $ T.pack $ show x]) (priority entry)
+
+buildSitemap :: Sitemap -> XML.Document
+buildSitemap sitemap = case XML.fromXMLDocument (X.Document (X.Prologue [] Nothing []) urlset []) of
+  Right doc -> doc
+  Left _ -> error "malformed document"
+  where
+    urlset = X.Element "urlset" [("xmlns", ["http://www.sitemaps.org/schemas/sitemap/0.9"])] $ map entryToXML (urls sitemap)
+
+renderSitemap :: Sitemap -> L.Text
+renderSitemap sitemap = renderSitemapWith XML.def sitemap
+
+renderSitemapWith :: XML.RenderSettings -> Sitemap -> L.Text
+renderSitemapWith opts sitemap = XML.renderText opts (buildSitemap sitemap)
